5623a2465519e746ab7a83d76ad3c7cf005a3362
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-16 03:17:42 +0200

    added Testimonial section

--- src/pages/Home/Fetured/Featured.jsx
+++ src/pages/Home/Fetured/Featured.jsx
@@ -4,27 +4,31 @@ import "./Featured.css";
 
 const Featured = () => {
   return (
-    <section className="featured-item text-white pt-8 my-10">
-      <SectionTitle
-        subHeading={"Check It Out"}
-        heading={"From Our Menu"}
-      ></SectionTitle>
-      <div className="md:flex justify-center items-center pt-12 pb-20 px-36">
-        <div>
-          <img src={featuredImg} alt="" />
-        </div>
-        <div className="md:ml-10">
-          <p>Aug 20, 2029</p>
-          <p className="uppercase">Where can i get some?</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-            atque eos vel voluptates quaerat asperiores ab molestiae quas fugit
-            doloremque eum delectus laudantium ratione repellat cum placeat est
-            maxime saepe molestias! Ab nihil dolore ex aut culpa enim, omnis
-            consequatur doloribus. Totam accusantium ipsa labore tempore porro
-            aliquam laboriosam blanditiis.
-          </p>
-          <button className="btn btn-outline">Order Now</button>
+    <section className="featured-item bg-fixed text-white my-20">
+      <div className="bg-opacity-60 pt-10 bg-black">
+        <SectionTitle
+          subHeading={"Check It Out"}
+          heading={"From Our Menu"}
+        ></SectionTitle>
+        <div className="md:flex justify-center items-center pt-12 pb-20 px-36">
+          <div>
+            <img src={featuredImg} alt="" />
+          </div>
+          <div className="md:ml-10">
+            <p>Aug 20, 2029</p>
+            <p className="uppercase">Where can i get some?</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+              atque eos vel voluptates quaerat asperiores ab molestiae quas
+              fugit doloremque eum delectus laudantium ratione repellat cum
+              placeat est maxime saepe molestias! Ab nihil dolore ex aut culpa
+              enim, omnis consequatur doloribus. Totam accusantium ipsa labore
+              tempore porro aliquam laboriosam blanditiis.
+            </p>
+            <button className="btn border-0 rounded-none border-b-4 btn-outline">
+              Order Now
+            </button>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "@smastrom/react-rating/style.css";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { Rating } from "@smastrom/react-rating";
+
+const Testimonials = () => {
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setReviews(data);
+      });
+  }, []);
+  return (
+    <section className="my-20">
+      <SectionTitle
+        heading={"Testimonials"}
+        subHeading={"What our Clients Say"}
+      ></SectionTitle>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {reviews.map((review) => (
+          <SwiperSlide key={review._id}>
+            <div className="text-center mx-24 m-16">
+              <Rating
+                className="mx-auto"
+                style={{ maxWidth: 180 }}
+                readOnly
+                value={review.rating}
+              />
+              <p className="py-8">{review.details}</p>
+              <h3 className="text-2xl text-orange-400">{review.name}</h3>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
+};
+
+export default Testimonials;
