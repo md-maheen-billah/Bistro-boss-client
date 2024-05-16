dd8277426f87f5f406e8cc377e26b8215a59be42
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-16 02:04:18 +0200

    first commit

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,56 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <NavLink>Item 1</NavLink>
+      </li>
+      <li>
+        <NavLink>Item 2</NavLink>
+      </li>
+      <li>
+        <NavLink>Item 3</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div className="navbar fixed z-10 max-w-screen-xl bg-opacity-30 bg-black">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navOptions}
+          </ul>
+        </div>
+        <a className="btn btn-ghost text-xl">Bistro Boss</a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Button</a>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"Popular Items"}
+        heading={"From Our Menu"}
+      ></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
