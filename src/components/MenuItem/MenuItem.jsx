1652594dcf576a602a7d5804283a84577346cfe4
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-16 02:19:36 +0200

    added from our menu section

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,15 @@
+const MenuItem = ({ item }) => {
+  const { image, price, recipe, name } = item;
+  return (
+    <div className="flex space-x-4">
+      <img className="w-[120px]" src={image} alt="" />
+      <div>
+        <h3 className="uppercase">{name} ------------</h3>
+        <p>{recipe}</p>
+      </div>
+      <p className="text-yellow-500">${price}</p>
+    </div>
+  );
+};
+
+export default MenuItem;
