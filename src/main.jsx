9ba7a323b5f9c9ae4babceda5400aae1930c4015
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-20 00:28:32 +0200

    used query to fidn cart fo specific user

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,20 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/PublicRoutes";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./providers/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <AuthProvider>
-        <div className="max-w-screen-xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
-      </AuthProvider>
-    </HelmetProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
+          <div className="max-w-screen-xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </HelmetProvider>
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
