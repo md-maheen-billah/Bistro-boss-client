8b99887324734f7a1b74ee638a2b15f8c480dd84
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-23 01:26:17 +0200

    implemented saving user to database when registering or login with google only once

--- src/components/SocialLogin/SocialLogin.jsx
+++ src/components/SocialLogin/SocialLogin.jsx
@@ -1,10 +1,33 @@
 import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../hooks/useAuth";
+import useAxiosPublic from "../../hooks/useAxiosPublic";
+import Swal from "sweetalert2";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
   const { signInWithGoogle } = useAuth();
+  const axiosPublic = useAxiosPublic();
   const handleGoogleLogIn = () => {
-    signInWithGoogle().then((result) => console.log(result.user));
+    signInWithGoogle().then((result) => {
+      const userInfo = {
+        name: result.user?.displayName,
+        email: result.user?.email,
+      };
+      axiosPublic.post("/users", userInfo).then((res) => {
+        console.log(res);
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Logged In Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate(from, { replace: true });
+      });
+    });
   };
   return (
     <div>
