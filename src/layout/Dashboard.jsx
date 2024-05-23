457c2e040f44c5dd2609b0b40559cebd172f1f3c
md-maheen-billah <md.maheen.billah@icloud.com>
2024-05-23 02:01:13 +0200

    used tanstack query to fetch user data

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,43 +1,82 @@
 import {
   FaAd,
+  FaBook,
   FaCalendar,
+  FaEnvelope,
   FaHome,
   FaList,
   FaSearch,
   FaShoppingCart,
+  FaUsers,
+  FaUtensils,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
+  // todo:get isAdmin value from the database
+  const isAdmin = true;
   return (
     <div className="flex">
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4 dark:text-black">
-          <li>
-            <NavLink to="/dashboard/userHome">
-              <FaHome /> User Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/reservation">
-              <FaCalendar /> Reservation
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/cart">
-              <FaShoppingCart /> My Cart
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/review">
-              <FaAd /> Add Review
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/dashboard/bookings">
-              <FaList /> My Bookings
-            </NavLink>
-          </li>
+          {isAdmin ? (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHome /> Admin Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/addItems">
+                  <FaUtensils /> Add Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageItems">
+                  <FaList /> Manage Items
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaBook /> Manage Bookings
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/users">
+                  <FaUsers /> All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <NavLink to="/dashboard/userHome">
+                  <FaHome /> User Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/reservation">
+                  <FaCalendar /> Reservation
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  <FaShoppingCart /> My Cart
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/review">
+                  <FaAd /> Add Review
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/bookings">
+                  <FaList /> My Bookings
+                </NavLink>
+              </li>
+            </>
+          )}
+          {/* shared nav links */}
           <div className="divider text-black divider-neutral"></div>
           <li>
             <NavLink to="/">
@@ -47,6 +86,11 @@ const Dashboard = () => {
           <li>
             <NavLink to="http://localhost:5173/order/salad">
               <FaSearch /> Menu
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="http://localhost:5173/contact">
+              <FaEnvelope /> Contact
             </NavLink>
           </li>
         </ul>
